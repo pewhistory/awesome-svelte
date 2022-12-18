de0721618dcefd26dff5ce9375e2db69ca1ba3f3
Pew <113262098+pewhistory@users.noreply.github.com>
2022-12-18 01:17:33 +0100

    configured tailwindcss

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,23 @@
+import sveltePreprocess from "svelte-preprocess";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-static";
 
 export default {
-    // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-    // for more information about preprocessors
-    preprocess: vitePreprocess(),
-}
+    // added these lines:
+    preprocess: [
+        vitePreprocess(),
+        sveltePreprocess({
+            postcss: true,
+        }),
+    ],
+    kit: {
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: "#svelte",
+        adapter: adapter({
+            // default options are shown
+            pages: "build",
+            assets: "build",
+            fallback: null,
+        }),
+    },
+};
